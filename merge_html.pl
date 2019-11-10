a664cce8cca18d8d73bdfd1c2e046cb88db42011
Matija Nalis <mnalis-git@voyager.hr>
2019-11-10 14:23:52 +0100

    start merging CSS+JS+HTML into one HTML

---
+++ merge_html.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl -wT
+# Matija Nalis GPLv3+, reads HTML from STDIN, and outputs merged HTML+JS+CSS to STDOUT
+#
+use strict;
+use autodie qw(:all);
+
+sub insert_file($$) {
+	my ($tag, $filename) = @_;
+
+	print "<$tag>\n";
+	open my $include, '<', $filename;
+	while (<$include>) {
+		print;
+	}
+	return "</$tag>\n";
+}
+
+while (<STDIN>) {
+	s{<link href="(.*?)" rel="stylesheet">}{insert_file('style', $1)}ge;	# insert CSS
+	s{<script src="(.*?)"></script>}{insert_file('script', $1)}ge;		# insert JS
+	print;
+}
